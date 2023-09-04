5cdc35dfeabeb5b986471866b62d085be935eed5
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2023-09-03 17:30:22 -0700

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,65 @@
 import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import "./Footer.css";
+import { BsFacebook, BsInstagram } from 'react-icons/bs';
+import logo from "../images/logo.png";
 
 const Footer = () => {
+    const navigate = useNavigate();
+    const scrollToHome = (event) => {
+        event.preventDefault();
+        navigate('/');
+        window.scrollTo(0, 0);
+    };
     return (
         <>
-        <Footer className='footer'>
+        <footer className='footer'>
             <div className='footer-container'>
-                
+                <div className="logo-container">
+                    <Link to="/" className='footer-logo' onClick={scrollToHome}>
+                        <img src={logo} width='120px' height='auto' max-width='100%' max-height='100%' alt='Logo' />
+                    </Link>
+                </div>
+
+                <div className='footer-menu'>
+                    <h1 className='menu-title'>Al Huda</h1>
+                    <ul>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/programs">Programs</Link></li>
+                        <li><Link to="/donate">Prayer Times</Link></li>
+                    </ul> 
+                </div>
+
+                <div className='footer-menu'>
+                    <h1 className='menu-title'>Resources</h1>
+                    <ul>
+                        <li><Link to="/donate">Mental help</Link></li>
+                        <li><Link to="/contact">Youth</Link></li>
+                        <li><Link to="/contact">Contact Us</Link></li>
+                    </ul>
+                </div>
+
+                <div className='get-in-touch'>
+                    <h1 className='menu-title'> Get in touch </h1>
+                    <div className='socials'>
+                        <a href="https://www.facebook.com/people/Quality-Designs/61550453621801/?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer"><BsFacebook /></a>
+                        <a href="https://www.instagram.com/qd_designs/" target="_blank" rel="noopener noreferrer"><BsInstagram /></a>
+                    </div>
+                </div>
+                <div className='line'></div>
+
+            </div>
+            <div className="copyright">
+                    <p>©{new Date().getFullYear()} Masjid Al Huda. All Rights Reserved.</p>
             </div>
             
-        </Footer>
+        </footer>
         
         </>
 
     );
 
 }
+
 
 export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import "./Home.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookQuran } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 
 const Home = () => {
     const navigate = useNavigate();
@@ -25,11 +28,37 @@ const Home = () => {
 
         {/* services */}
         <section className='services'>
-
+            <h1 className='services-title'>Our Services</h1>
+            <p className='services-description'>Serving Our Community</p>
+            <div className='services-container'>
+                <div className='services-box'>
+                    <FontAwesomeIcon icon={faBookQuran} style={{ color: '#038000' }} />
+                    <h1 className='services-box-title'>Quran Program</h1>
+                    <p className='services-box-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Quisquam, voluptate.</p>
+                </div>
+                <div className='services-box'>
+                    <FontAwesomeIcon icon={farHeart} style={{ color: '#038000' }} />
+                    <h1 className='services-box-title'>Nikkah Services</h1>
+                    <p className='services-box-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Quisquam, voluptate.</p>
+                </div>
+                <div className='services-box'>
+                    <h1 className='services-box-title'>Halaqas</h1>
+                    <p className='services-box-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Quisquam, voluptate.</p>
+                </div>
+                <div className='services-box'>
+                    <h1 className='services-box-title'>Funeral service</h1>
+                    <p className='services-box-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Quisquam, voluptate.</p>
+                </div>
+            </div>
         </section>
 
         {/* prayer times */}
         <section className='prayer'>
+            
 
 
         </section>
@@ -37,7 +66,18 @@ const Home = () => {
 
         {/* gallery */}
         <section className='gallery'>
-
+            {/* <h1 className='gallery-title'>Gallery</h1>
+            <div className='gallery-container'>
+                <div className='gallery-box'>
+                    <img src='https://images.unsplash.com/photo-1581093458794-8d1f0b3b6c7c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9zY2lkJTIwY2FyJTIwYmVzdCUyMG1vcmUlMjBzaG9ydCUyMGNvbXBsZXRlJTIwY29tcGFueXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80' alt='gallery' />
+                </div>
+                <div className='gallery-box'>
+                    <img src='https://images.unsplash.com/photo-1581093458794-8d1f0b3b6c7c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9zY2lkJTIwY2FyJTIwYmVzdCUyMG1vcmUlMjBzaG9ydCUyMGNvbXBsZXRlJTIwY29tcGFueXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80' alt='gallery' />
+                </div>
+                <div className='gallery-box'>
+                    <img src='https://images.unsplash.com/photo-1581093458794-8d1f0b3b6c7c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9zY2lkJTIwY2FyJTIwYmVzdCUyMG1vcmUlMjBzaG9ydCUyMGNvbXBsZXRlJTIwY29tcGFueXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80' alt='gallery' />
+                </div>
+             </div> */}
         </section>
 
         </>
