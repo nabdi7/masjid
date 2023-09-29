571bf12ff339aed8aad03170573eceb8e7ed3f25
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2023-09-28 20:40:14 -0700

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,7 +50,7 @@ const Footer = () => {
 
             </div>
             <div className="copyright">
-                    <p>Designed by Quality Designs || ©{new Date().getFullYear()} Masjid Al Huda. All Rights Reserved.</p>
+                    <p>Made by <a href="https://qualitydesigns.site/" target="_blank" rel="noopener noreferrer" style={{ color: 'black', fontWeight: 'bold'}}>Quality Designs</a> || ©{new Date().getFullYear()} Masjid Al Huda. All Rights Reserved.</p>
             </div>
             
         </footer>
